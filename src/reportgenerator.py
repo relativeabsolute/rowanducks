dd587b2a4d00a390a5b1ae9e528088ff018b67b5
Johan Burke <jburke4126@gmail.com>
2017-02-16 16:15:42 -0500

    Added skeleton of ReportGenerator class.  This is mostly an example commit to ensure team members can access the GitHub correctly.

---
+++ src/reportgenerator.py
@@ -0,0 +1,7 @@
+# Author: Johan Burke
+
+class ReportGenerator:
+    """Class used to compare new and old versions of source files in the specified language"""
+
+    def __init__(self):
+        self.reportStr = ""
