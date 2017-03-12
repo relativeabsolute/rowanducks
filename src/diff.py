9273681ebfbe5d94b11ac43feb689a6e1aa73a72
Thomas <thomas@perka.com>
2017-03-12 14:27:36 -0400

    Adds,mods,deletes between two fils now counted accurately.

--- src/diff.py
+++ src/diff.py
@@ -1,19 +1,36 @@
 import difflib
+import re
 
 file1 = "CMS-2_HighLevel_2.txt"
 file2 = "CMS-2_HighLevel_Edited.txt"
 
 diff = difflib.ndiff(open(file1).readlines(),open(file2).readlines())
 
-print ''.join(diff)
-
+# Accepts a '+' followed by a space and any number of any characters until line ends to extract in-line comment.
+addition_pattern = '(\+ .*)'
 additions = 0
-modifications = 0
+# Accepts a '-' followed by a space and any number of any characters until line ends to extract in-line comment.
+deletion_pattern = '(\- .*)'
 deletions = 0
+# Accepts a '?' followed by a space and any number of any characters until line ends to extract in-line comment.
+modification_pattern = '(\? .*)'
+modifications = 0
 
-# Accepts a '+' followed by any number of any characters until line ends to extract in-line comment.
-addition_pattern = '(+. .*)'
-# Accepts a '-' followed by any number of any characters until line ends to extract in-line comment.
-deletion_pattern = '(-. .*)'
-# Accepts a '?' followed by any number of any characters until line ends to extract in-line comment.
-modification_pattern = '(?. .*)'
+for n in diff:
+    if re.search(addition_pattern, n):
+        additions+=1
+    elif re.search(deletion_pattern, n):
+        deletions+=1
+    elif re.search(modification_pattern, n):
+        modifications+=1
+# Addition/deletion patterns also show for modifications. Prevent double counting.
+additions-=modifications
+deletions-=modifications
+
+print
+print("Additions: " + str(additions))
+print
+print("Modifications: " + str(modifications))
+print
+print ("Deletions: " + str(deletions))
+print
