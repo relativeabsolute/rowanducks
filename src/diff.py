616ece8afea692217b10aa3c54248ce3e916b8d3
Thomas <thomas@perka.com>
2017-03-12 12:50:11 -0400

    Added regex for additions, modifications, deletions

--- src/diff.py
+++ src/diff.py
@@ -6,3 +6,14 @@ file2 = "CMS-2_HighLevel_Edited.txt"
 diff = difflib.ndiff(open(file1).readlines(),open(file2).readlines())
 
 print ''.join(diff)
+
+additions = 0
+modifications = 0
+deletions = 0
+
+# Accepts a '+' followed by any number of any characters until line ends to extract in-line comment.
+addition_pattern = '(+. .*)'
+# Accepts a '-' followed by any number of any characters until line ends to extract in-line comment.
+deletion_pattern = '(-. .*)'
+# Accepts a '?' followed by any number of any characters until line ends to extract in-line comment.
+modification_pattern = '(?. .*)'
