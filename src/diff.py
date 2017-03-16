00b65c4039f349da0bb3739740ba9828663670f2
Johan Burke <jburke4126@gmail.com>
2017-03-16 14:36:22 -0400

    Encapsulated diff into a class.

--- src/diff.py
+++ src/diff.py
@@ -1,36 +1,47 @@
 import difflib
 import re
 
-file1 = "CMS-2_HighLevel_2.txt"
-file2 = "CMS-2_HighLevel_Edited.txt"
+class Diff:
+    # TODO: define module associated with the files
+    def __init__(self, file1, file2):
+        self.file1 = file1
+        self.file2 = file2
+    
+    def run_diff(self):
+        self.diff = difflib.ndiff(open(file1).readlines(),open(file2).readlines())
 
-diff = difflib.ndiff(open(file1).readlines(),open(file2).readlines())
+        # Accepts a '+' followed by a space and any number of any characters until line ends to extract in-line comment.
+        addition_pattern = '(\+ .*)'
+        self.additions = 0
+        # Accepts a '-' followed by a space and any number of any characters until line ends to extract in-line comment.
+        deletion_pattern = '(\- .*)'
+        self.deletions = 0
+        # Accepts a '?' followed by a space and any number of any characters until line ends to extract in-line comment.
+        modification_pattern = '(\? .*)'
+        self.modifications = 0
 
-# Accepts a '+' followed by a space and any number of any characters until line ends to extract in-line comment.
-addition_pattern = '(\+ .*)'
-additions = 0
-# Accepts a '-' followed by a space and any number of any characters until line ends to extract in-line comment.
-deletion_pattern = '(\- .*)'
-deletions = 0
-# Accepts a '?' followed by a space and any number of any characters until line ends to extract in-line comment.
-modification_pattern = '(\? .*)'
-modifications = 0
+        for n in self.diff:
+            if re.search(addition_pattern, n):
+                self.additions+=1
+            elif re.search(deletion_pattern, n):
+                self.deletions+=1
+            elif re.search(modification_pattern, n):
+                self.modifications+=1
+        # Addition/deletion patterns also show for self.modifications. Prevent double counting.
+        self.additions-=self.modifications
+        self.deletions-=self.modifications
 
-for n in diff:
-    if re.search(addition_pattern, n):
-        additions+=1
-    elif re.search(deletion_pattern, n):
-        deletions+=1
-    elif re.search(modification_pattern, n):
-        modifications+=1
-# Addition/deletion patterns also show for modifications. Prevent double counting.
-additions-=modifications
-deletions-=modifications
+    def __str__(self):
+        result = "Additions: " + str(self.additions) + "\n"
+        result += "Modifications: " + str(self.modifications) + "\n"
+        result += "Deletions: " + str(self.deletions) + "\n"
+        return result
 
-print
-print("Additions: " + str(additions))
-print
-print("Modifications: " + str(modifications))
-print
-print ("Deletions: " + str(deletions))
-print
+if __name__ == "__main__":
+    file1 = "CMS-2_HighLevel_2.txt"
+    file2 = "CMS-2_HighLevel_Edited.txt"
+
+    d = Diff(file1, file2)
+    d.run_diff()
+    print("Diff results")
+    print(str(d))
