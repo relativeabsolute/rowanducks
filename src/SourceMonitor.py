07cc1b3384e0eba0f173f0e845f73e7738e4220d
Johan Burke <jburke4126@gmail.com>
2017-04-01 13:50:09 -0400

    Added current progress on formatting.

--- src/SourceMonitor.py
+++ src/SourceMonitor.py
@@ -12,19 +12,33 @@ class SourceMonitor:
 
         date_title = now.strftime("%d/%b/%Y")
 
-        self.header_string += "\t\t\t\t\t\t\t\t\t\t\t" + date_title
-        self.header_string += "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tPAGE 1\n"
+        #self.header_string += "\t\t\t\t\t\t\t\t\t\t\t" + date_title
+        #self.header_string += "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tPAGE 1\n"
+
+        self.header_string = ("{0:<" + str(int(self.columns / 2)) + "}{1:>" + str(int(self.columns / 2)) + "}").format('SRCMON - SOURCE CODE MONITOR, VERSION 07.00', date_title)
+        self.header_string += ("\n{0:>" + str(self.columns) + "}").format('PAGE 1')
 
         class_name = "///CLASS NAME HERE///"
-        self.header_string += "\t\t\t\t\t\t\t" + class_name + "\n"
+        self.header_string += ("\n{0:^" + str(self.columns) + "}").format(class_name)
+        self.header_string += "\n"
+    
 
-        column_names = "MODULE\t\t\t\t\t|FILE STATUS|\tINITIAL SIZE\t| CPCR\t\t\t"
-        column_names += "ADDITIONS\tMODIFICATIONS\t|\tDELETIONS\n"
+        column_names = ("{0:<" + str(int(self.columns / 4)) + "}").format('MODULE')
+        column_names += ("{0:^" + str(int(self.columns / 12)) + "}").format('|FILE STATUS|')
+
+        #column_names = "\nMODULE\t\t\t\t\t|FILE STATUS|\tINITIAL SIZE\t| CPCR\t\t\t"
+        #column_names += "ADDITIONS\tMODIFICATIONS\t|\tDELETIONS\n"
+    
+        column_names += ("{0:^" + str(int(self.columns / 8)) + "}").format('INITIAL SIZE')
+        column_names += ("| {0:<" + str(int(self.columns / 8)) + "}").format('CPCR')
+
+        column_names += "ADDITIONS"
+
         self.header_string += column_names
 
-        underline_str = "----------------------------------------|-----------|-------------------|"
-        underline_str += "-------------------------------------------------------|----------------"
-        self.header_string += underline_str
+        #underline_str = "----------------------------------------|-----------|-------------------|"
+        #underline_str += "-------------------------------------------------------|----------------"
+        #self.header_string += underline_str
 
     def get_rows(self):
         # main string contains the rows of data of the report
@@ -35,6 +49,7 @@ class SourceMonitor:
 
 
     def __init__(self, diffs):
+        self.columns = 160
         self.get_header_string()
         self.diffs = diffs
         self.get_rows()
