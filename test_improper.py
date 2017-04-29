8634ab433bd972b0334f6d3dc243a1a6b4f81eb1
ChrisCurreri <curreric0@students.rowan.edu>
2017-04-28 22:51:01 -0400

    Commiting so progress is saved on GitHub

---
+++ test_improper.py
@@ -0,0 +1,16 @@
+# script used to test improper cases
+# Author: Chris Curreri
+
+from unittest import TestLoader, TestResult
+
+def run_improper():
+    return
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    run_improper()
