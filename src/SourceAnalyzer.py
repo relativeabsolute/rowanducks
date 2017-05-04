8224f45be0747a5f8a69c62abb464764cbd1751e
Johan Burke <jburke4126@gmail.com>
2017-05-04 15:38:59 -0400

    Updated source analyzer to output data

--- src/SourceAnalyzer.py
+++ src/SourceAnalyzer.py
@@ -1,5 +1,6 @@
 # Author: Johan Burke
 
+import os.path
 import sys
 from .regex import main as regex_main
 from .CMS2File import CMS2File
@@ -27,57 +28,81 @@ class SourceAnalyzer:
 
         self._header_str += "{0:^{width}}\n".format('Source Analysis Summary', width=int(self.columns))
         
-        under_str = "{0:{fill}<{width}}".format("", fill=" ", width=int(self.columns/10))
+        under_str = "{0:{fill}<{width}}".format("", fill=" ", width=int(self.columns/4))
         under_str += "+{0:{fill}^{width}}+".format("High Level CMS-2", fill="-", width=int(self.columns/2) - 5)
         under_str += " +{0:{fill}^{width}}+".format("CMS-2 DIRECT", fill="-", width=int(self.columns / 6) + 1)
         under_str += "  +{0:{fill}^{width}}+".format("Total", fill="-", width=int(self.columns / 10))
 
         self._header_str += under_str + "\n"
 
-        column_headings = "{0:<{width}}".format('Component', width=int(self.columns/10))
-        column_headings += "{0:<{width}}".format('Exec', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('Exec', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('Data', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('Data', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('Comment', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('Comment', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('NonCmt', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('Other', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('Exec', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('Data', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('Comment', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('Exec', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('Source', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('CSWTC', width=int(self.columns/15))
-        column_headings += "{0:<{width}}".format('MX Delimt', width=int(self.columns/15))
+        column_headings = "{0:<{width}}".format('Component', width=int(self.columns/5))
+        column_headings += "{0:{fill}^{width}}".format("", fill=" ", width=int(self.columns/20))
+        column_headings += "{0:<{width}}".format('Exec', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('Exec', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('Data', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('Data', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('Comment', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('Comment', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('NonCmt', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('Other', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('Exec', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('Data', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('Comment', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('Exec', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('Source', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('CSWTC', width=int(self.columns/16))
+        column_headings += "{0:<{width}}".format('MX Delimt', width=int(self.columns/16))
 
         self._header_str += column_headings + '\n'
         
-        column_types = "{0:<{width}}".format('Name', width=int(self.columns/20))
+        column_types = "{0:<{width}}".format('Name', width=int(self.columns/5))
         column_types += "{0:<{width}}".format('Type', width=int(self.columns/20))
-        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('Lines', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('Lines', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('Lines', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('Lines', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('Lines', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('Lines', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/15))
-        column_types += "{0:<{width}}".format('LV Stmts', width=int(self.columns/15))
+        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('Lines', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('Lines', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('Lines', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('Lines', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('Lines', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('Lines', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('Stmts', width=int(self.columns/16))
+        column_types += "{0:<{width}}".format('LV Stmts', width=int(self.columns/16))
         
         self._header_str += column_types + '\n'
         self._header_str += "{0:{fill}^{width}}".format("", fill="-", width=int(self.columns))
 
+    def __set_data_str(self):
+        for info in self.__file_list:
+            info.recalculate_totals()
+            current_str = "{0:<{width}}".format(os.path.split(info.name)[1], width=int(self.columns/5))
+            #current_str += "{0:<{width}}".format(info.type, width=int(self.columns/20))
+            current_str += "{0:<{width}}".format("SYST", width=int(self.columns/20))
+            current_str += "{0:<{width}}".format(info.hl_exec_stmts, width=int(self.columns/16))
+            current_str += "{0:<{width}}".format(info.hl_exec_lines, width=int(self.columns/16))
+            current_str += "{0:<{width}}".format(info.hl_data_stmts, width=int(self.columns/16))
+            current_str += "{0:<{width}}".format(info.hl_data_lines, width=int(self.columns/16))
+            current_str += "{0:<{width}}".format(info.block_comments, width=int(self.columns/16))
+            current_str += "{0:<{width}}".format(info.block_comment_lines, width=int(self.columns/16))
+            current_str += "{0:<{width}}".format(info.hl_noncomment_lines, width=int(self.columns/16))
+            #current_str += "{0:<{width}}".format(info.hl_other_stmts, width=int(self.columns/16))
+            current_str += "{0:<{width}}".format('0', width=int(self.columns/16))
+            current_str += "{0:<{width}}".format(info.direct_exec_stmts, width=int(self.columns/16))
+            #current_str += "{0:<{width}}".format(info.direct_data_stmts, width=int(self.columns/16))
+            current_str += "{0:<{width}}".format('0', width=int(self.columns/16))
+            current_str += "{0:<{width}}".format(info.direct_comments, width=int(self.columns/16))
+            current_str += "{0:<{width}}".format(info.total_exec_stmts, width=int(self.columns/16))
+            current_str += "{0:<{width}}".format(info.total_src_lines, width=int(self.columns/16))
+
+            self._data_str += current_str + '\n'
 
     def __format(self):
         self.__set_header_str()
-        #self.__set_data_str()
+        self.__set_data_str()
 
     def __init__(self, file_list, columns = 160):
         self._header_str = ""
@@ -88,11 +113,11 @@ class SourceAnalyzer:
         self.__format()
 
     def __str__(self):
-        return self.header_str
+        return self.header_str + '\n' + self.data_str
 
 def main(args):
     file_list = regex_main(args)
-    san = SourceAnalyzer(args)
+    san = SourceAnalyzer(file_list)
     print(str(san))
 
 if __name__ == "__main__":
