92b900c087cef387446703835958780267d99f67
ChrisCurreri <curreric0@students.rowan.edu>
2017-05-05 18:09:58 -0400

    Added necessary txt files and class framework for TestImproper.py

---
+++ src/testing/source_analyzer/test_improper.py
@@ -0,0 +1,39 @@
+# script used to test improper cases (see verification Script Steps: 1.5.1-1.5.4)
+# Author: Chris Curreri
+
+import unittest
+from src.regex import main
+
+class ImproperTestCase(unittest.TestCase):
+
+    # 1.5.1 Run test_bad_ext...ok
+    # Will print ok if and only if the number of files
+    # with incorrect extensions match the ones found
+    # in num_bad_ext.txt.
+    def test_bad_ext(self):
+        return
+
+    # 1.5.2 test_misplace_end_sys...ok
+    # Will print ok if and only if the number of instances
+    # of misplaced "END-SYSTEM" statements match the number
+    # found in num_misplaced_end_sys.txt.
+    def test_misplace_end_sys(self):
+        return
+
+    # 1.5.3 test_multi_component...ok
+    # Will print ok if and only if the number matches
+    # the number found in num_multi_component.txt.
+    def test_multi_component(self):
+        return
+
+
+    # 1.5.4 test_name_mismatch...ok
+    # Will print ok if and only if the number of
+    # mismatches between component name and filename
+    # match the number in num_name_mismatch.txt.
+    def test_name_mismatch(self):
+        return
+
+if __name__ == "__main__":
+    print("Running TestImproper:")
+    unittest.main()
