0395f2abb735f2f0b8f2ffe8653d7a8b6b3e0471
Johan Burke <jburke4126@gmail.com>
2017-05-06 14:50:53 -0400

    Updated run_tests to successfully be able to find files

--- run_tests.py
+++ run_tests.py
@@ -2,24 +2,22 @@
 # Author: Johan Burke
 
 from unittest import TestLoader, TestResult
-from run_tests import run_tests
 
 def run_tests():
     loader = TestLoader()
-    suite = loader.discover('.')
+    suite = loader.discover('src/testing/')
     result = TestResult()
     result = suite.run(result)
     print("Errors:")
 
     """ Check for Erorrs """
     for err in result.errors:
-        print("Where: " + str(err[0]))
-        print("Error: " + err[1])
+        print("\tWhere: " + str(err[0]))
+        print("\tError: " + err[1])
     print("Failures:")
     for fail in result.failures:
-        print(str(fail))
-    for fail in run_tests():
-        print(str(fail))
+        print("\tTest case: {0}".format(str(fail[0])))
+        print("\tFailure string: {0}".format(fail[1]))
 
 if __name__ == "__main__":
     run_tests()
