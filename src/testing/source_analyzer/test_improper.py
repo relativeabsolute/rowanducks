091485c7cbd2efde668167697553f86a71709dd2
ChrisCurreri <curreric0@students.rowan.edu>
2017-05-05 22:47:33 -0400

    Added TODO for test_improper.

--- src/testing/source_analyzer/test_improper.py
+++ src/testing/source_analyzer/test_improper.py
@@ -11,28 +11,31 @@ class ImproperTestCase(unittest.TestCase):
     # with incorrect extensions match the ones found
     # in num_bad_ext.txt.
     def test_bad_ext(self):
-        return
+        # TODO check if bad extensions are the same
+        pass
 
     # 1.5.2 test_misplace_end_sys...ok
     # Will print ok if and only if the number of instances
     # of misplaced "END-SYSTEM" statements match the number
     # found in num_misplaced_end_sys.txt.
     def test_misplace_end_sys(self):
-        return
+        # TODO check if end system states are correct
+        pass
 
     # 1.5.3 test_multi_component...ok
     # Will print ok if and only if the number matches
     # the number found in num_multi_component.txt.
     def test_multi_component(self):
-        return
-
+        # TODO check if multiple compontents are correct
+        pass
 
     # 1.5.4 test_name_mismatch...ok
     # Will print ok if and only if the number of
     # mismatches between component name and filename
     # match the number in num_name_mismatch.txt.
     def test_name_mismatch(self):
-        return
+        # TODO check if the name and filename match the number here
+        pass
 
 if __name__ == "__main__":
     print("Running TestImproper:")
